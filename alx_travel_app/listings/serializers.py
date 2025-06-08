4147bb81c6e6638ee8f5f98af008aee938e2096b
calebomariba <calebomariba1993@gmail.com>
2025-06-08 14:38:14 +0300

    Add serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,13 +1,17 @@
-from django.db import models
 from rest_framework import serializers
-from .models import Listing, Booking
+from .models import Listing, Booking, Review
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = '__all__'  # Includes all fields: title, description, price_per_night, location, available, created_at
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = '__all__'  # Includes all fields: listing, user_name, check_in, check_out, total_price, created_at
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'  # Includes all fields: listing, user_name, rating, comment, created_at
