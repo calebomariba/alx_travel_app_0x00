01fb2a2253ec4a0eb611c7597a8288c310a05272
calebomariba <calebomariba1993@gmail.com>
2025-06-09 11:26:38 +0300

    Task 1: Added viewsets, configured URLs, and updated README for API development

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,7 +1,12 @@
-# alx_travel_app/listings/urls.py
-from django.urls import path
-from . import views
+# alx_travel_app_0x01/alx_travel_app/listings/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    # Add API endpoints here later
+    path('', include(router.urls)),
 ]
